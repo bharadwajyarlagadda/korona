ae55eb49844c92493570c9e0f7962a06357b9163
bharadwajyarlagadda <yarlagaddabharadwaj@gmail.com>
2016-08-21 09:49:31 -0400

    Added attributes for <area> tag

--- korona/html/attributes.py
+++ korona/html/attributes.py
@@ -88,6 +88,68 @@ TAG_ATTRIBUTES = {
                 'values': None
             }
         },
+    'area':
+        {
+            'alt': {
+                'description': 'Specifies an alternate text for the area. '
+                               'Required if the href attribute is present',
+                'values': None
+            },
+            'coords': {
+                'description': 'Specifies the coordinates of the area',
+                'values': None
+            },
+            'download': {
+                'description': 'Specifies that the target will be downloaded '
+                               'when a user clicks on the hyperlink',
+                'values': None
+            },
+            'href': {
+                'description': 'Specifies the hyperlink target for the area',
+                'values': None
+            },
+            'hreflang': {
+                'description': 'Specifies the language of the target URL',
+                'values': None
+            },
+            'media': {
+                'description': 'Specifies what media/device the target URL is '
+                               'optimized for',
+                'values': None
+            },
+            'nohref': {
+                'description': 'Specifies that an area has no associated link',
+                'values': None
+            },
+            'rel': {
+                'description': 'Specifies the relationship between the current'
+                               ' document and the target URL',
+                'values': ['alternate',
+                           'author',
+                           'bookmark',
+                           'help',
+                           'license',
+                           'next',
+                           'nofollow',
+                           'noreferrer',
+                           'prefetch',
+                           'prev',
+                           'search',
+                           'tag']
+            },
+            'shape': {
+                'description': 'Specifies the shape of the area',
+                'values': ['default', 'rect', 'circle', 'poly']
+            },
+            'target': {
+                'description': 'Specifies where to open the target URL',
+                'values': None
+            },
+            'type': {
+                'description': 'Specifies the media type of the target URL',
+                'values': None
+            }
+        },
     'base':
         {
             'href': {
