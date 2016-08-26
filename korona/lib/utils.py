afc231d7614c607af12aa9b581ede9f0b2a45a9e
bharadwajyarlagadda <yarlagaddabharadwaj@gmail.com>
2016-08-26 00:29:10 -0400

    Created method for validating the tag's attribute values

--- korona/lib/utils.py
+++ korona/lib/utils.py
@@ -11,3 +11,14 @@ def validate_tag(tag=None):
 
     if tag not in TAGS:
         raise ValueError('{0} tag is not supported')
+
+
+def validate_tag_attribute_value(tag, value):
+    """Validates whether the given value is a string or not."""
+    if not value:
+        return
+
+    # If the attribute value is not a string raise a ValueError.
+    if not isinstance(value, str):
+        raise ValueError('<{tag}>: {value} should be a string'
+                         .format(tag=tag, value=value))
