dae408258ae3fa0e53d29fde190569457c5f662d
bharadwajyarlagadda <yarlagaddabharadwaj@gmail.com>
2016-08-27 17:36:00 -0400

    Added template for <button> tag

--- korona/templates/html/__init__.py
+++ korona/templates/html/__init__.py
@@ -7,6 +7,7 @@ from .tags import (
     area_tag,
     bold_tag,
     base_tag,
+    button_tag,
     canvas_tag,
     caption_tag,
     cite_tag
--- korona/templates/html/tags.py
+++ korona/templates/html/tags.py
@@ -49,6 +49,21 @@ base_tag = env.from_string("""
       {% if target -%} target="{{ target }}" {% endif -%}>
 """)
 
+button_tag = env.from_string("""
+<button {% if name -%} name="{{ name }}" {% endif -%}
+        {% if type -%} type="{{ type }}" {% endif -%}
+        {% if value -%} value="{{ value }}" {% endif -%}
+        {% if form -%} form="{{ form }}" {% endif -%}
+        {% if formaction -%} formaction="{{ formaction }}" {% endif -%}
+        {% if formenctype -%} formenctype="{{ formenctype }}" {% endif -%}
+        {% if formmethod -%} formmethod="{{ formmethod }}" {% endif -%}
+        {% if formtarget -%} formtarget="{{ formtarget }}" {% endif -%}
+        {% if formnovalidate -%} formnovalidate {% endif -%}
+        {% if disabled -%} disabled {% endif -%}
+        {% if autofocus -%} autofocus {% endif -%}>
+        {% if text -%} "{{ text }}" {% endif -%}</button>
+""")
+
 canvas_tag = env.from_string("""
 <canvas {% if height -%} height="{{ height }}" {% endif -%}
         {% if width -%} width="{{ width }}" {% endif -%}></canvas>
