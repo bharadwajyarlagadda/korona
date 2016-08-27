acbd6b33a2316cfc7a3b9ddbe572fdbb19114ec0
bharadwajyarlagadda <yarlagaddabharadwaj@gmail.com>
2016-08-27 17:37:04 -0400

    Added Button() class for constructing <button> tag; Added tests for the same; Fixed #10

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -9,6 +9,7 @@ from korona.html.construct import (
     Area,
     B,
     Base,
+    Button,
     Canvas,
     Caption,
     Cite
@@ -20,6 +21,7 @@ from korona.templates.html import (
     area_tag,
     bold_tag,
     base_tag,
+    button_tag,
     canvas_tag,
     caption_tag,
     cite_tag
@@ -288,6 +290,64 @@ def test_construct_base_tag_error(attributes, exception, error_msg):
     """Test for validating base tag's attributes."""
     with pytest.raises(exception) as exc:
         Base(**attributes)
+
+    assert error_msg in str(exc)
+
+
+@parametrize('attributes', [
+    ({'type': 'submit', 'autofocus': True}),
+    ({'type': 'submit', 'disabled': True}),
+    ({'type': 'submit', 'formnovalidate': True}),
+    ({'type': 'submit', 'text': 'HTML'}),
+    ({'type': 'submit', 'name': 'HTML', 'value': 'HTML', 'text': 'HTML'}),
+    ({'type': 'submit',
+      'text': 'HTML',
+      'form': 'form1',
+      'formaction': 'demo.asp',
+      'formmethod': 'post',
+      'formtarget': '_blank',
+      'formenctype': 'multipart/form-data'})
+])
+def test_construct_button_tag(attributes):
+    """Test for validating whether the button tag is constructed correctly or
+    not.
+    """
+    button = Button(**attributes)
+    assert button.construct_tag() == button_tag.render(attributes)
+
+
+@parametrize('attributes,exception,error_msg', [
+    ({}, AttributeError, 'Button type should be specified'),
+    ({'type': 'reset', 'formaction': 'demo.asp'},
+     AttributeError,
+     'attribute is only used for buttons with type "submit"'),
+    ({'type': 'reset', 'formmethod': 'post'},
+     AttributeError,
+     'attribute is only used for buttons with type "submit"'),
+    ({'type': 'reset', 'formtarget': '_blank'},
+     AttributeError,
+     'attribute is only used for buttons with type "submit"'),
+    ({'type': 'reset', 'formenctype': 'multipart/form-data'},
+     AttributeError,
+     'attribute is only used for buttons with type "submit"'),
+    ({'type': 'reset', 'formnovalidate': True},
+     AttributeError,
+     'attribute is only used for buttons with type "submit"'),
+    ({'type': 'abc'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'type': 'submit', 'formenctype': 'abc'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'type': 'submit', 'formmethod': 'abc'},
+     AttributeError,
+     'attribute values should be one of these'),
+
+])
+def test_construct_button_tag_error(attributes, exception, error_msg):
+    """Test for validating button tag's attributes."""
+    with pytest.raises(exception) as exc:
+        Button(**attributes)
 
     assert error_msg in str(exc)
 
