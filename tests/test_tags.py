5aa308ad5b29550d0ba3737b6c1af1f6515afc78
bharadwajyarlagadda <yarlagaddabharadwaj@gmail.com>
2016-08-29 23:33:47 -0400

    Added ColGroup() class for constructing the <colgroup></colgroup> tag; Added tests for the same

--- tests/test_tags.py
+++ tests/test_tags.py
@@ -15,7 +15,8 @@ from korona.html.construct import (
     Canvas,
     Caption,
     Cite,
-    Col
+    Col,
+    ColGroup
 )
 from korona.templates.html import (
     anchor_tag,
@@ -30,7 +31,8 @@ from korona.templates.html import (
     canvas_tag,
     caption_tag,
     cite_tag,
-    col_tag
+    col_tag,
+    colgroup_tag
 )
 from korona.lib.utils import validate_tag
 
@@ -473,5 +475,39 @@ def test_construct_col_tag_error(attributes, exception, error_msg):
     """Test for validating col tag's attributes."""
     with pytest.raises(exception) as exc:
         Col(**attributes)
+
+    assert error_msg in str(exc)
+
+
+@parametrize('attributes', [
+    ({'align': 'char'}),
+    ({'align': 'char', 'char': '.'}),
+    ({'align': 'char', 'char': '.', 'charoff': '2'}),
+    ({'align': 'left', 'span': '2'}),
+    ({'align': 'right', 'valign': 'top'}),
+    ({'width': '130'})
+])
+def test_construct_colgroup_tag(attributes):
+    """Test for validating whether the colgroup tag is constructed correctly or
+    not.
+    """
+    colgroup = ColGroup(**attributes)
+    assert colgroup.construct() == colgroup_tag.render(attributes)
+
+
+@parametrize('attributes,exception,error_msg', [
+    ({'char': '.'}, AttributeError, 'The char attribute can only be used'),
+    ({'charoff': '2'}, AttributeError, 'The charoff attribute can only be'),
+    ({'char': '.', 'charoff': '2'},
+     AttributeError,
+     'The char attribute can only be used'),
+    ({'align': 'left', 'charoff': '2'},
+     AttributeError,
+     'The charoff attribute can only be')
+])
+def test_construct_colgroup_tag_error(attributes, exception, error_msg):
+    """Test for validating colgroup tag's attributes."""
+    with pytest.raises(exception) as exc:
+        ColGroup(**attributes)
 
     assert error_msg in str(exc)
