058943475a91bad5d7b6cdcf5fa9c7f6ac9f88e4
bharadwajyarlagadda <yarlagaddabharadwaj@gmail.com>
2016-09-04 10:28:38 -0400

    Changed the version to 0.3.0-alpha

--- korona/__pkg__.py
+++ korona/__pkg__.py
@@ -5,7 +5,7 @@
 __package_name__ = 'korona'
 __description__ = 'Package for building html pages'
 __url__ = 'https://github.com/bharadwajyarlagadda/korona'
-__version__ = '0.2.0'
+__version__ = '0.3.0-alpha'
 __author__ = 'Bharadwaj Yarlagadda'
 __email__ = 'yarlagaddabharadwaj@gmail.com'
 __license__ = 'MIT License'
