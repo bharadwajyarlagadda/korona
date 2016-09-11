87da437ca39b986c16c0d4d6c6d51bbf366a406f
Bharadwaj <yarlagaddabharadwaj@gmail.com>
2016-09-10 23:25:37 -0400

    Added IFrame() class for constructing <iframe> tag; Added tests for the same

--- korona/html/construct.py
+++ korona/html/construct.py
@@ -49,7 +49,8 @@ from ..templates.html import (
     header_tag,
     hr_tag,
     html_tag,
-    italics_tag
+    italics_tag,
+    iframe_tag
 )
 
 RECTANGLE_SHAPE_COORDINATES = 4
@@ -1410,3 +1411,91 @@ class I(object):
     def construct(self):
         """Returns the constructed italics tag <i>."""
         return italics_tag.render(self.values)
+
+
+class IFrame(object):
+    """Class for constructing <iframe> tag.
+
+    Args:
+        align (str): Specifies the alignment of an <iframe> according to
+            surrounding elements.
+        frameborder (str): Specifies whether or not to display a border around
+            an <iframe>.
+        height (str): Specifies the height of an <iframe>.
+        longdesc (str): Specifies a page that contains a long description of
+            the content of an <iframe>.
+        marginheight (str): Specifies the top and bottom margins of the
+            content of an <iframe>.
+        marginwidth (str): Specifies the left and right margins of the content
+            of an <iframe>.
+        name (str): Specifies the name of an <iframe>.
+        sandbox (str): Enables an extra set of restrictions for the content in
+            an <iframe>.
+        scrolling (str): Specifies whether or not to display scrollbars in an
+            <iframe>.
+        src (str): Specifies the address of the document to embed in the
+            <iframe>.
+        srcdoc (str): Specifies the HTML content of the page to show in the
+            <iframe>.
+        width (str): Specifies the width of an <iframe>.
+
+    .. versionadded:: 0.4.0-dev
+    """
+    def __init__(self,
+                 align=None,
+                 frameborder=None,
+                 height=None,
+                 longdesc=None,
+                 marginheight=None,
+                 marginwidth=None,
+                 name=None,
+                 sandbox=None,
+                 scrolling=None,
+                 src=None,
+                 srcdoc=None,
+                 width=None):
+        self.tag = 'iframe'
+        validate_attribute_values(tag=self.tag,
+                                  attribute_name='align',
+                                  value=align)
+        validate_attribute_values(tag=self.tag,
+                                  attribute_name='frameborder',
+                                  value=frameborder)
+        validate_url(attribute_name='longdesc', url=longdesc)
+        self.validate_sandbox(sandbox=sandbox)
+        validate_attribute_values(tag=self.tag,
+                                  attribute_name='scrolling',
+                                  value=scrolling)
+        validate_url(attribute_name='src', url=src)
+        self.values = {'align': align,
+                       'frameborder': frameborder,
+                       'height': height,
+                       'longdesc': longdesc,
+                       'marginheight': marginheight,
+                       'marginwidth': marginwidth,
+                       'name': name,
+                       'sandbox': sandbox,
+                       'scrolling': scrolling,
+                       'src': src,
+                       'srcdoc': srcdoc,
+                       'width': width}
+
+    def construct(self):
+        """Returns the constructed iframe tag <iframe>."""
+        return iframe_tag.render(self.values)
+
+    def validate_sandbox(self, sandbox):
+        """Validates sandbox attribute. The value of the sandbox attribute
+        can either be just sandbox (then all restrictions are applied), or a
+        space-separated list of pre-defined values that will REMOVE the
+        particular restrictions.
+        """
+        if not sandbox:
+            return
+
+        parts = sandbox.split(' ')
+
+        for part in parts:
+            validate_attribute_values(tag=self.tag,
+                                      attribute_name='sandbox',
+                                      value=part)
--- tests/test_tags.py
+++ tests/test_tags.py
@@ -41,7 +41,8 @@ from korona.html.construct import (
     Header,
     HR,
     Html,
-    I
+    I,
+    IFrame
 )
 from korona.templates.html import (
     anchor_tag,
@@ -82,7 +83,8 @@ from korona.templates.html import (
     header_tag,
     hr_tag,
     html_tag,
-    italics_tag
+    italics_tag,
+    iframe_tag
 )
 from korona.lib.utils import validate_tag
 
@@ -1027,3 +1029,49 @@ def test_construct_italics_tag(attributes):
     """
     italics = I(**attributes)
     assert italics.construct() == italics_tag.render(attributes)
+
+
+@parametrize('attributes', [
+    ({'align': 'left', 'frameborder': '1'}),
+    ({'src': '/demo.asp', 'height': '100', 'width': '200'}),
+    ({'longdesc': 'demo.txt'}),
+    ({'marginheight': '50', 'marginwidth': '50', 'name': 'example'}),
+    ({'sandbox': 'allow-forms allow-scripts', 'scrolling': 'yes'}),
+    ({'srcdoc': '<p>hello</p>'})
+])
+def test_construct_iframe_tag(attributes):
+    """Test for validating whether the iframe tag is constructed correctly or
+    not.
+    """
+    iframe = IFrame(**attributes)
+    assert iframe.construct() == iframe_tag.render(attributes)
+
+
+@parametrize('attributes,exception,error_msg', [
+    ({'frameborder': '4'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'align': 'left-center'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'sandbox': 'abc'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'sandbox': 'allow-forms abc'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'scrolling': 'mount'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'sandbox': 'allow-forms allow-pointer-lock abc'},
+     AttributeError,
+     'attribute values should be one of these'),
+    ({'longdesc': 123}, ValueError, 'is not a valid url'),
+    ({'src': 123}, ValueError, 'is not a valid url')
+])
+def test_construct_iframe_tag_error(attributes, exception, error_msg):
+    """Test for validating iframe tag's attributes."""
+    with pytest.raises(exception) as exc:
+        IFrame(**attributes)
+
+    assert error_msg in str(exc)
