0554edd15144ffa4ba4deac0a53a9ef41922cf99
Bharadwaj <yarlagaddabharadwaj@gmail.com>
2016-09-26 02:19:38 +0000

    Added level as arg in the method warn()

--- korona/warnings.py
+++ korona/warnings.py
@@ -6,7 +6,7 @@ import warnings
 STACK_LEVEL = 3
 
 
-def warn(warning):
+def warn(warning, level=STACK_LEVEL):
     """Displays warning messages to the user."""
     warnings.simplefilter('always', UserWarning)
-    warnings.warn(warning, stacklevel=STACK_LEVEL)
+    warnings.warn(warning, stacklevel=level)
